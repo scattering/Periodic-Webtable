a152f3d00df21ac246e168e1db07c0d886236c93
Brian Maranville <brian.maranville@nist.gov>
2012-04-11 15:31:15 -0400

    added JS database of periodictable values, as well as python script to generate this from periodictable python module

---
+++ js/make_periodic_json.py
@@ -0,0 +1,42 @@
+# run this one first, to update the overall code from the base ("periodic_table.html")
+# then run add_xSLD_toperiodic.py to add the xray bits.
+
+import periodictable as pt
+import simplejson
+
+#sa = open('periodic_table.html', 'r').readlines()
+#sout = open('periodic_table.json', 'w')
+wl = 1.5418 # copper K-alpha
+
+by_el_name = {}
+by_symbol = {}
+ptable = []
+
+for n in range(1,119):
+    #n = el.number
+    if n in pt.elements._element:
+        el = pt.elements[n]
+        if not hasattr(el, 'symbol'): continue
+        item = {
+            "symbol": el.symbol,
+            "name": el.name,
+            "number": el.number,
+            "nSLD": el.neutron.sld()[0],
+            "xSLD": el.xray.sld(wavelength=wl)[0],
+            "b_c": el.neutron.b_c,
+            "b_p": el.neutron.bp,
+            "density": el.density,
+            "number_density": el.number_density *1e-22 if el.number_density else None
+            }
+    else: item = {}
+    ptable.append(item)
+    
+simplejson.dump(ptable, open('periodic_table.json', 'w'), indent=4)
+jspt = simplejson.dumps(ptable, indent=4)
+with open('periodic_table.js', 'w') as jsout:
+    jsout.write("elements = ")
+    jsout.write(jspt)
+    
+
+        
+    
